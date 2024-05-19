871b67f4ba054c4c648e3e092cee65ef107371e5
Ghozy Abdurrahman <106439922+ZerXXX0@users.noreply.github.com>
2024-05-19 17:16:53 +0700

    update main.go
    add the interface

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//interface
+	fmt.Println("============================================")
+	fmt.Println("   Welcome to Aplikasi Inventori Barang")
+	fmt.Println("============================================")
+
+	i := 0
+	for i != 1 {
+		fmt.Println("============================================")
+		fmt.Println("                   MENU")
+		fmt.Println("1. Tambah Barang")
+		fmt.Println("2. Hapus Barang")
+		fmt.Println("3. Lihat Inventori")
+		fmt.Println("0. Exit")
+	}
 }
