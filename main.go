0d75da7e3e6259e5c6ebfa327abdf6e0baf28a44
Ghozy Abdurrahman <106439922+ZerXXX0@users.noreply.github.com>
2024-05-20 00:49:25 +0700

    Update main.go
    added procedures, switch-case in menu, struct and array for barang, etc

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const NMAX int = 100
+
+type dataBarang [NMAX]struct {
+	nama        string
+	tanggal, id int
+}
+
 func main() {
 	//interface
 	fmt.Println("============================================")
@@ -10,11 +17,42 @@ func main() {
 
 	i := 0
 	for i != 1 {
+		//interface
 		fmt.Println("============================================")
 		fmt.Println("                   MENU")
 		fmt.Println("1. Tambah Barang")
 		fmt.Println("2. Hapus Barang")
 		fmt.Println("3. Lihat Inventori")
 		fmt.Println("0. Exit")
+
+		//variable
+		var userInput, jumlah int
+		var barang dataBarang
+
+		//scan input dari user
+		fmt.Scan(&userInput)
+		//switch-case untuk menu
+		switch {
+		case userInput == 1:
+			fmt.Scan(&jumlah)
+			tambahBarang(&barang, &jumlah)
+			fmt.Println("Barang berhasil ditambahkan")
+		case userInput == 2:
+			var idDelete int
+			fmt.Scan(&idDelete)
+			hapusBarang(&barang, &jumlah, idDelete)
+			fmt.Println("Barang berhasil dihapus")
+		case userInput == 3:
+			lihatInventori(barang, jumlah)
+		}
 	}
+}
+func tambahBarang(barang *dataBarang, jumlah *int) {
+
+}
+func hapusBarang(barang *dataBarang, jumlah *int, idDelete int) {
+
+}
+func lihatInventori(barang dataBarang, jumlah int) {
+
 }
